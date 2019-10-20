ac56b37c7cb96622c76b81c2dfff4643ab5c03da
Alexey Dzyuba <a.dzyuba@gmail.com>
2019-10-20 06:45:43 +0200

    polish examples

--- stat/fitter_bukin.py
+++ stat/fitter_bukin.py
@@ -10,13 +10,16 @@ ds = rfile["ds"]
 im = ROOT.RooRealVar ('im'   , 'im'                 ,  2.240 , 2.330 )
 pk = ROOT.RooRealVar ('pk'   , 'peak   '   , 2.2875 ,  2.286 , 2.289 )
 sg = ROOT.RooRealVar ('sg'   , 'sigma'     , 0.0045 ,  0.003 , 0.010 )
+rl = ROOT.RooRealVar ('rl' , 'rhoL'        , 0.0000 ,  0.000 , 2.000 )
+rr = ROOT.RooRealVar ('rr' , 'rhoR'        , 0.0000 ,  0.000 , 2.000 )
+xi = ROOT.RooRealVar ('xi' , 'xi'          , 0.0000 , -0.010 , 0.010 )
 #-------------------------------------------------------------------------------
-sig_ga = Models.Gauss_pdf( 'sig_ga', xvar=im,   mean=pk, sigma=sg )
-bkg0 = Models.Bkg_pdf ( 'bkg0' , xvar = im , power = 0 )
-model= Models.Fit1D   ( signal = sig_ga , background = bkg0 )
+sig_bu = Models.Bukin_pdf( 'sig_bu', xvar=im, mean=pk, sigma=sg, xi=xi,
+                            rhoL=rl, rhoR=rr)
+bkg0   = Models.Bkg_pdf ( 'bkg0' , xvar = im , power = 0 )
+model  = Models.Fit1D   ( signal = sig_bu , background = bkg0 )
 #-------------------------------------------------------------------------------
 dh = ( ds.reduce( ROOT.RooArgSet( im ) , "im>0" ) ).binnedClone()
-#-------------------------------------------------------------------------------
 with timing():
     r, w = model.fitTo( dh )
     r, w = model.fitTo(ds, draw=True, nbins=100, ncpu=4)
@@ -25,5 +28,5 @@ print(r)
 #-------------------------------------------------------------------------------
 h = w.pullHist()
 draw_param( r, w, h, 90, im, 0.06*ds.sumEntries(), name="Lc", XTitle ="Mass",
-                Prefix="Gauss" , Type="png", var_Units = "GeV/c^{2}")
+                Prefix="Bukin" , Type="png", var_Units = "GeV/c^{2}")
 #-------------------------------------------------------------------------------
