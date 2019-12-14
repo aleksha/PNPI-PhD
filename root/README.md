def0cbc2931a29db01f294808d7db7e16e630065
Alexey Dzyuba <a.dzyuba@gmail.com>
2019-12-14 17:41:58 +0100

    how to compile program

---
+++ root/README.md
@@ -0,0 +1,14 @@
+Recepies for CERN ROOT
+======================
+
+How to compile standalone ROOT programm
+---------------------------------------
+
+See:
+
+ - https://en.wikibooks.org/wiki/ROOT/Getting_Started/Many_Ways_to_Use_ROOT
+
+```bash
+g++ -o hello hello.cc `root-config --cflags --glibs`
+```
+
---
+++ root/hello.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include "TCanvas.h"
+#include "TH1F.h"
+#include "TRandom.h"
+
+
+using namespace std;
+
+TCanvas *canv;
+TH1F *h;
+
+void test()
+{
+  TRandom *rnd = new TRandom(time(0));
+  Double_t x = rnd->Rndm();
+  cout << "x = " << x << endl;
+}
+
+void hello()
+{
+  cout << "hello world!" << endl;
+}
+
+#ifndef __CINT__
+int main()
+{
+  hello();
+  test();
+  int a;
+  canv = new TCanvas();
+  h = new TH1F("h",";x;y",10,0,10);
+  h->Fill(5.5);
+  h->Draw();
+  canv->Update();
+  canv->Print("canv.png");
+  cin >> a ;
+  canv->Close();
+  return 0;
+}
+#endif
